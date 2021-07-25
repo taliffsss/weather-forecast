9a6b41e31841728b4f5b4976ce92766b008db55d
Mark Anthony Naluz <anthony.naluz15@gmail.com>
2021-07-25 17:53:15 +0800

    from fetch to get

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
-
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
 const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
 
 const cDate = new Date();
